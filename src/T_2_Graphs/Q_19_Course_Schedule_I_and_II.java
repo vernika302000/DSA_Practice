528e2964949c89ab9e50003e7bd061a0efdb13a3
vernika302000 <vvernika.30@gmail.com>
2023-12-23 18:49:28 +0530

    Course schedule I and II

--- src/T_2_Graphs/Q_19_Course_Schedule_I_and_II.java
+++ src/T_2_Graphs/Q_19_Course_Schedule_I_and_II.java
@@ -9,6 +9,38 @@ public class Q_19_Course_Schedule_I_and_II
         int n=scn.nextInt();
         int m=scn.nextInt();
         List<List<Integer>> adj=new LinkedList<>();
+        Q_16_Topological_Sorting.buildDirectedGraph(adj,n,m);
+        int[] indegree=new int[n];
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++)
+        {
+            for(int it:adj.get(i))
+            {
+                indegree[it]++;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0) q.add(i);
+        }
+        List<Integer> topo=new ArrayList<>();
+        while(!q.isEmpty())
+        {
+            int node=q.peek();
+            q.remove();
+            topo.add(node);
+            for(int it:adj.get(node))
+            {
+                indegree[it]--;
+                if(indegree[it]==0) q.add(it);
+            }
+        }
+        if(topo.size()==n)
+        {
+            for(int val:topo) System.out.println(val+" ");
+            return;
+        }
+        System.out.println("Not Possible");
 
 
     }
