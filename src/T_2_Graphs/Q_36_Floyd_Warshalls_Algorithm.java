f716952482c51394b06ca052b7f1cc52a9632461
vernika302000 <vvernika.30@gmail.com>
2024-01-08 09:38:29 +0530

    Floyd warshall's algorithm

--- src/T_2_Graphs/Q_36_Floyd_Warshalls_Algorithm.java
+++ src/T_2_Graphs/Q_36_Floyd_Warshalls_Algorithm.java
@@ -18,13 +18,29 @@ public class Q_36_Floyd_Warshalls_Algorithm
         {
             for(int j=0;j<m;j++) mat[i][j]=scn.nextInt();
         }
-        int[][] cost=new int[n][m];
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(mat[i][j]==-1) cost[i][j]=(int)(1e9);
-                if(i==j) cost[i][j]=0;
+                if(mat[i][j]==-1) mat[i][j]=(int)(1e9);
+                if(i==j) mat[i][j]=0;
+            }
+        }
+        for(int k=0;k<n;k++)
+        {
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    mat[i][j]=Math.min(mat[i][j],mat[i][k]+mat[k][j]);
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mat[i][j]==(int)(1e9)) mat[i][j]=-1;
             }
         }
 
