68081637e9908a28b0597a56e3b26d0d139c87c7
vernika302000 <vvernika.30@gmail.com>
2024-01-18 09:46:47 +0530

    Cherry Pickup problem

--- src/T_3_Dynamic_Programming/Q_14_Cherry_Pickup_Problem.java
+++ src/T_3_Dynamic_Programming/Q_14_Cherry_Pickup_Problem.java
@@ -31,6 +31,28 @@ public class Q_14_Cherry_Pickup_Problem
         }
         return maxi;
     }
+    public static int solve_DP(int i,int j1,int j2,int[][] matrix,int[][] dp)
+    {
+        if(j1<0 || j1>=matrix[0].length || j2<0 || j2>=matrix[0].length) return (int)(-1e8);
+        if(i==matrix.length-1)
+        {
+            if(j1==j2) return matrix[i][j1];
+            else return matrix[i][j1]+matrix[i][j2];
+        }
+        int maxi=(int)(-1e8);
+        for(int dj1=-1;dj1<=1;dj1++)
+        {
+            for(int dj2=-1;dj2<=1;dj2++)
+            {
+                int value=0;
+                if(j1==j2) return matrix[i][j1];
+                else value= matrix[i][j1]+matrix[i][j2];
+                value+=solve_DP(i+1,j1+dj1,j2+dj2,matrix,dp);
+                maxi=Math.max(maxi,value);
+            }
+        }
+        return maxi;
+    }
     public static void main(String[] args)
     {
         Scanner scn=new Scanner(System.in);
@@ -43,6 +65,9 @@ public class Q_14_Cherry_Pickup_Problem
         }
 
         System.out.println(solve(0,0,c-1,grid)); //simple recursive approach
+
+        int[][] dp=new int[r][c];
+        System.out.println(solve_DP(0,0,c-1,grid,dp));
 
 
 
